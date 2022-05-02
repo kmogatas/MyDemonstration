ad4adbc4b7c67490721c29758868f0c3dcedcc59
kmogatas <kaleighrossmogatas@gmail.com>
2022-05-01 18:58:17 -0700

    Added test file

--- MyDemonstration.java
+++ MyDemonstration.java
@@ -1,5 +1,17 @@
+
 public class MyDemonstration{
 
-// VSCode
+    public static int basicallyMultiplication(int num, int num2){
+
+        int total = 0;
+
+        for(int i = 0; i < 5; i++){
+            total = (num + num2);
+        }
+
+        return total;
+    }
+
+    //There is a bug.
 
 }
---
+++ MyDemonstrationTest.java
@@ -0,0 +1,11 @@
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class MyDemonstrationTest {
+    
+    @Test 
+    public void basicallyMultiplication(){
+        assertEquals(15, MyDemonstration.basicallyMultiplication(1, 2));
+    }
+}
